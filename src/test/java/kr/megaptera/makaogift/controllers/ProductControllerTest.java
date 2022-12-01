d975e766c50202abc01edc014e8fee0000dc4345
Bohyun Kang <bbohyun@kakao.com>
2022-12-01 14:31:23 +0900

    Backdoor API 구현(/setup-database)
    - 기본 DB 세팅

--- src/test/java/kr/megaptera/makaogift/controllers/ProductControllerTest.java
+++ src/test/java/kr/megaptera/makaogift/controllers/ProductControllerTest.java
@@ -17,7 +17,6 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
