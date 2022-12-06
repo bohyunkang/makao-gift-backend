b16fd5ce2de910495adba46c39953c60d1f82e23
Bohyun Kang <bbohyun@kakao.com>
2022-12-07 03:08:54 +0900

    product -> products로 변수명 수정
    - ProductsDto 변수명 수정

--- src/main/java/kr/megaptera/makaogift/dtos/ProductsDto.java
+++ src/main/java/kr/megaptera/makaogift/dtos/ProductsDto.java
@@ -3,13 +3,16 @@ package kr.megaptera.makaogift.dtos;
 import java.util.List;
 
 public class ProductsDto {
-    private List<ProductDto> product;
+    private List<ProductDto> products;
 
-    public ProductsDto(List<ProductDto> product) {
-        this.product = product;
+    public ProductsDto() {
     }
 
-    public List<ProductDto> getProduct() {
-        return product;
+    public ProductsDto(List<ProductDto> products) {
+        this.products = products;
+    }
+
+    public List<ProductDto> getProducts() {
+        return products;
     }
 }
